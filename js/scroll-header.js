7cf76977d755d14a44979d8d1b9a1c0de9d37bab
Maksim Lisovoi <lisovoi123@gmail.com>
2022-08-03 19:07:38 +0300

    made func hiding header & added footer 1 part

--- js/scroll-header.js
+++ js/scroll-header.js
@@ -1,13 +1,21 @@
-const className = 'inverted';
-const scrollTrigger = 60;
+const $header = document.querySelector('[js-header]');
 
-const header = document.querySelector('.header');
+var prevScrollY = 0;
+var minScrollY = 100;
 
-window.onscroll = function () {
-  // We add pageYOffset for compatibility with IE.
-  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
-    header.classList.add(className);
-  } else {
-    header.classList.remove(className);
+window.addEventListener('scroll', function (e) {
+  // console.log(window.scrollY);
+  if (window.scrollY < prevScrollY) {
+    $header.classList.add('header--visible');
+  } else if (window.scrollY > prevScrollY) {
+    if (window.scrollY < minScrollY) return;
+    $header.classList.remove('header--visible');
   }
-};
+  if (window.scrollY === 0) {
+    $header.classList.add('header--transparent-bg');
+  } else {
+    $header.classList.remove('header--transparent-bg');
+  }
+
+  prevScrollY = window.scrollY;
+});
