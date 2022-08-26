495640ee5a4d043b8362de68e02ca2abe7e86d07
Maksim Lisovoi <lisovoi123@gmail.com>
2022-08-26 15:55:51 +0300

    header scroll try to fix

--- js/scroll-header.js
+++ js/scroll-header.js
@@ -1,20 +1,21 @@
 const $header = document.querySelector('[js-header]');
 
-var prevScrollY = 0;
-var minScrollY = 100;
+let prevScrollY = 0;
+let minScrollY = 100;
 
 window.addEventListener('scroll', function (e) {
   // console.log(window.scrollY);
+
+  if (window.scrollY === 0) {
+    $header.classList.add('header--transparent-bg');
+  } else {
+    $header.classList.remove('header--transparent-bg');
+  }
   if (window.scrollY < prevScrollY) {
     $header.classList.add('header--visible');
   } else if (window.scrollY > prevScrollY) {
     if (window.scrollY < minScrollY) return;
     $header.classList.remove('header--visible');
-  }
-  if (window.scrollY === 0) {
-    $header.classList.add('header--transparent-bg');
-  } else {
-    $header.classList.remove('header--transparent-bg');
   }
 
   prevScrollY = window.scrollY;
